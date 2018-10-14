f4b6976f9580129c8901f62f6e18b795569cac49
maldworth <m@maldworth.com>
2018-10-14 10:47:26 -0400

    Add Tests for 2.38.00 and 2.40.00

---
+++ src/Aldsoft.Acord.LA.Tests/2.40.00/LifeBuilderTests.cs
@@ -0,0 +1,78 @@
+namespace Aldsoft.Acord.LA.Tests.V2_40_00
+{
+    extern alias V2_40_00;
+    using V2_40_00::Aldsoft.Acord.LA;
+    using Xunit;
+    using System;
+
+    public class LifeBuilderTests
+    {
+        [Fact]
+        public void V2_40_00_LifeBuilder_NoBaseCoverage()
+        {
+            // Arrange
+            var builder = Life_Type.CreateBuilder();
+
+            // Act/Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => builder.Build());
+            Assert.Equal("You cannot build the Life_Type entity without a base coverage.", ex.Message);
+        }
+
+        [Fact]
+        public void V2_40_00_LifeBuilder_CannotConstructWithCoverage()
+        {
+            // Arrange/Act/Assert
+            var ex = Assert.Throws<ArgumentException>(() => Life_Type.CreateBuilder(new Life_Type { Coverage = { new Coverage_Type() } }));
+            Assert.Equal("The Life_Type.Coverage collections should not be set. This builder will help handle the ACORD rules with setting this property.\r\nParameter name: baseEntity", ex.Message);
+        }
+
+        [Fact]
+        public void V2_40_00_LifeBuilder_CannotBuildWithCoverage()
+        {
+            // Arrange
+            var builder = Life_Type.CreateBuilder();
+            builder.AddBaseCoverage(new Coverage_Type());
+
+            // Act/Assert
+            var ex = Assert.Throws<ArgumentException>(() => builder.Build(new Life_Type { Coverage = { new Coverage_Type() } }));
+            Assert.Equal("The Life_Type.Coverage collections should not be set. This builder will help handle the ACORD rules with setting this property.\r\nParameter name: baseEntity", ex.Message);
+        }
+
+        [Fact]
+        public void V2_40_00_LifeBuilder_ConstructWithLife()
+        {
+            // Arrange
+            var builder = Life_Type.CreateBuilder(new Life_Type { id = "Life1" });
+            builder.AddBaseCoverage(new Coverage_Type());
+            var life = builder.Build();
+
+            // Act
+            string xmlString;
+            life.Serialize(out xmlString);
+
+            // Assert
+            Assert.Contains("id=\"Life1\"", xmlString);
+        }
+
+        [Fact]
+        public void V2_40_00_LifeBuilder_BuildWithLife()
+        {
+            // Arrange
+            var builder = Life_Type.CreateBuilder()
+                .AddBaseCoverage(new Coverage_Type { id="BaseCoverage2" })
+                .AddCoverage(new Coverage_Type { id = "Coverage2" });
+            var life = builder.Build(new Life_Type { id = "Life2" });
+
+            // Act
+            string xmlString;
+            life.Serialize(out xmlString);
+
+            // Assert
+            Assert.Contains("id=\"Life2\"", xmlString);
+            var indexOfBaseCoverage = xmlString.IndexOf("BaseCoverage2");
+            var indexOfCoverage = xmlString.IndexOf("Coverage2");
+            Assert.True(indexOfBaseCoverage < indexOfCoverage);
+            Assert.Contains("id=\"Life2\"", xmlString);
+        }
+    }
+}
---
+++ src/Aldsoft.Acord.LA.Tests/2.40.00/UserAuthRequestBuilderTests.cs
@@ -0,0 +1,67 @@
+namespace Aldsoft.Acord.LA.Tests.V2_40_00
+{
+    extern alias V2_40_00;
+    using V2_40_00::Aldsoft.Acord.LA;
+    using Xunit;
+    using System;
+
+    
+    public class UserAuthRequestBuilderTests
+    {
+        [Fact]
+        public void V2_40_00_Request_LoginName_Ordering_Test1()
+        {
+            // Arrange
+            var builder1 = UserAuthRequest_Type.CreateBuilder()
+                .UserAuthentication(new UserAuthentication_Type { id="512" })
+                .UserLoginName("testname")
+                .UserDomain("workspace");
+
+            var builder = TXLife_Type.CreateBuilder()
+                .Version("2.40.00")
+                .AddUserAuthRequest(builder1.Build());
+
+            var txLife = builder.Build();
+
+            // Act
+            string xmlString;
+            txLife.Serialize(out xmlString);
+
+            // Assert
+            var authRequestIndex = xmlString.IndexOf("<UserAuthRequest>");
+            var loginIndex = xmlString.IndexOf("<UserLoginName>testname</UserLoginName>");
+            var authIndex = xmlString.IndexOf("<UserAuthentication id=\"512\" />");
+
+            // Ensures the user login appears first
+            Assert.True(loginIndex < authIndex);
+        }
+
+        [Fact]
+        public void V2_40_00_Request_LoginName_Ordering_Test2()
+        {
+            // Arrange
+            var builder1 = UserAuthRequest_Type.CreateBuilder()
+                .UserPswd(new UserPswd_Type { CryptType = "asdf" })
+                .UserLoginName("testname")
+                .UserDomain("workspace");
+
+            var builder = TXLife_Type.CreateBuilder()
+                .Version("2.40.00")
+                .AddUserAuthRequest(builder1.Build());
+
+            var txLife = builder.Build();
+
+            // Act
+            string xmlString;
+            txLife.Serialize(out xmlString);
+
+            // Assert
+            var authRequestIndex = xmlString.IndexOf("<UserAuthRequest>");
+            var loginIndex = xmlString.IndexOf("<UserLoginName>testname</UserLoginName>");
+            var pswdIndex = xmlString.IndexOf("<UserPswd>");
+
+            // Ensures the user login appears first
+            Assert.True(loginIndex < pswdIndex);
+        }
+    }
+}
